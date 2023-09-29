6e0a646caf763902e20a0a23cd2f64f5862368af
smth_lya <korsakovdaniltankist@yandex.ru>
2023-09-29 23:34:45 +0300

    Добавлен метод для задачи: Rotate Matrix

--- CycleTasks/Program.cs
+++ CycleTasks/Program.cs
@@ -30,5 +30,67 @@ namespace Tasks.Cycle
             }
             return numInt;
         }
+        public static void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
+
+            for (int k = 0; k <= n / 2; k++)
+            {
+                for (int g = k; g < n - k - 1; g++)
+                {
+                    int curX = g,
+                        curY = k;
+
+                    int newX = curX,
+                        newY = curY;
+
+                    int offsetX = g - k,
+                        offsetY = n - 2 * k - 1 - offsetX;
+
+                    // make backup
+
+                    int backupCurXY = matrix[curY][curX];
+
+                    // 1 loop
+
+                    newX -= offsetX;
+                    newY += offsetY;
+
+                    matrix[curY][curX] = matrix[newY][newX];
+
+                    curX = newX;
+                    curY = newY;
+
+
+                    // 2 loop
+
+                    newX += offsetY;
+                    newY += offsetX;
+
+                    matrix[curY][curX] = matrix[newY][newX];
+
+                    curX = newX;
+                    curY = newY;
+
+
+                    // 3 loop
+
+                    newX += offsetX;
+                    newY -= offsetY;
+
+                    matrix[curY][curX] = matrix[newY][newX];
+
+                    curX = newX;
+                    curY = newY;
+
+
+                    // 4 loop
+
+                    matrix[curY][curX] = backupCurXY;
+
+                }
+            }
+        }
+      
     }
 }
