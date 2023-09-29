6b090f08beb0a3434ae6d770fbaba2d34a5f416f
smth_lya <korsakovdaniltankist@yandex.ru>
2023-09-29 23:32:40 +0300

    Добавлен метод для задачи: Roman to Integer

---
+++ CycleTasks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tasks.Cycle
+{
+    public class Program
+    {
+        public static int RomanToInt(string s)
+        {
+            int numInt = 0;
+
+            int curLetterInt = 0, prevLetterInt = 0;
+
+            foreach (char letter in s)
+            {
+                curLetterInt = letter switch
+                {
+                    'M' => 1000,
+                    'D' => 500,
+                    'C' => 100,
+                    'L' => 50,
+                    'X' => 10,
+                    'V' => 5,
+                    'I' => 1,
+                    _ => 0,
+                };
+
+                numInt += curLetterInt > prevLetterInt ? (-prevLetterInt) + (curLetterInt - prevLetterInt) : +curLetterInt;
+
+                prevLetterInt = curLetterInt;
+            }
+            return numInt;
+        }
+    }
+}
